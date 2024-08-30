09694f3a399a53d00ea82b31628d855c38ce85a7
Akash Prasad <akashprasadmk@gmail.com>
2024-08-31 03:37:47 +0530

    committing now

--- lib/auth/authview.dart
+++ lib/auth/authview.dart
@@ -9,11 +9,26 @@ class AuthPage extends StatelessWidget {
     final _screenwidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-          child: Container(
-            height: _screenheight,
-            width: _screenwidth,
-            child: ,
-          ),),
+        child: Container(
+          height: _screenheight,
+          width: _screenwidth,
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: _screenheight * 0.13814,
+              left: _screenwidth * 0.04843318,
+              right: _screenwidth * 0.04843318,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Sign in to Account',
+                  style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
